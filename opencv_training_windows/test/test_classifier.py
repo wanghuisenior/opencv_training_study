019b991ffaef8d9cc25d4461a32d527c44dbac1e
wanghui <154306777@qq.com>
2020-04-28 10:05:09 +0800

    改动

--- opencv_training_windows/test/test_classifier.py
+++ opencv_training_windows/test/test_classifier.py
@@ -1,32 +1,34 @@
 # -*- coding:utf8 -*-
 import os
-import cv2 #请确保已经安装了opencv-python ， pip install openc-python
+import cv2  # 请确保已经安装了opencv-python ， pip install openc-python
 
 # 测试图片
-imgName = "women.jpg"
+imgName = "w.jpg"
 # 训练得到的分类器xml文件，训练完成后在trained_classifiers文件夹下，即：params.xml
-xmlFileName = "cascade.xml"
+# xmlFileName = "cascade.xml"
+xmlFileName = "haarcascade_frontalface_alt.xml"
 if not (os.path.exists(imgName) and os.path.exists(xmlFileName)):
-	print("图片或检测器文件不存在")
+    print("图片或检测器文件不存在")
 # 矩形颜色和描边
 color = (0, 0, 255)  # 红色框
 strokeWeight = 1  # 线宽为 1
 
 # 测试训练的检测器是否可用
-windowName = "detect"
+windowName = "object detect"
 img = cv2.imread(imgName)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # 加载检测文件
 cascade = cv2.CascadeClassifier(xmlFileName)
 
-rects = cascade.detectMultiScale(img)
+rects = cascade.detectMultiScale(gray)
 if len(rects) > 0:
-	print('========================检测到了===============================')
+    print('========================检测到%d个目标===============================' % (len(rects)))
 else:
-	print('没检测到东西')
+    print('没检测到东西')
 # 获取矩形列表
 for x, y, width, height in rects:
-	cv2.rectangle(img, (x, y), (x + width, y + height), color, strokeWeight)
+    cv2.rectangle(img, (x, y), (x + width, y + height), color, strokeWeight)
 
 # 显示
 cv2.imshow(windowName, img)
